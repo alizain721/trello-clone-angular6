381ce9566276803af071ecc32ffbefc0074cafb7
ali zain <alizain721@gmail.com>
2018-10-14 21:46:19 +0500

    Add Login component to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
