748e31ce1e15fc5506c037d3aa74926c63d56b6f
ali zain <alizain721@gmail.com>
2018-10-19 08:43:24 +0500

    Edit

--- src/app/components/boards/boards.component.ts
+++ src/app/components/boards/boards.component.ts
@@ -11,28 +11,50 @@ export class BoardsComponent implements OnInit {
   username:string = ''
   boardTitle:string = 'To Do'
   boards:any = []
+  readOnly:any = []
+  butons:any = []
   message:string = ''
   displayTextArea:boolean = false;
-  currentMsg:string = '';
+  currentMsg:string = ''
+  editedText:string = ''
 
   constructor(private dataservice: DataService, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(params => {
-      console.log(params.get('id'));
+      console.log(params.get('id'))
     });
   }
 
   add() {
     this.boards.push(this.currentMsg);
-    this.dataservice.changeMessage(this.currentMsg);
-    this.displayTextArea = false;
-    this.currentMsg = '';
+    this.readOnly.push(true)
+    this.butons.push(false)
+    this.dataservice.changeMessage(this.currentMsg)
+    this.displayTextArea = false
+    this.currentMsg = ''
   }
 
   toggle() {
-    this.displayTextArea = !this.displayTextArea;
+    this.displayTextArea = !this.displayTextArea
+    this.currentMsg = ''
   }
+
+  edit(index) {
+    this.readOnly[index] = false
+    this.butons[index] = true
+
+  }
+
+  save(index) {
+    this.boards[index] = this.editedText
+    this.editedText = ''
+    this.displayTextArea = false
+    this.readOnly[index] = true
+    this.butons[index] = false
+  }
+
+  onKey(event) {this.editedText = event.target.value}
 
 }
